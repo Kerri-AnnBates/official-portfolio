397228107a3b943a1bfe24362b26af04be7a0a04
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-03 15:41:21 -0500

    base layout of nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/app.scss';
+import Header from './components/header/Header';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Test</h1>
+			<Header />
 		</div>
 	);
 }
---
+++ src/components/header/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<div className="nav-bar">
+			<div className="logo">
+				<div>img here</div>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
