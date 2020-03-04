afc2a6b8866dfc66fa40dff64d9ecf951915f2d2
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-04 11:45:25 -0500

    set up tech stack

--- src/components/main/TechDetails.js
+++ src/components/main/TechDetails.js
@@ -7,8 +7,15 @@ const TechDetails = () => {
 			<div className="tech-stack">
 				<Container fluid={true}>
 					<h2>Technology Stack</h2>
-					<div className="tech-grid">
-
+					<div className="tech-flex">
+						<div className="flex-item"><i class="fab fa-html5"></i></div>
+						<div className="flex-item"><i class="fab fa-css3-alt"></i></div>
+						<div className="flex-item"><i class="fab fa-sass"></i></div>
+						<div className="flex-item"><i class="fab fa-react"></i></div>
+						<div className="flex-item"><i class="fab fa-js"></i></div>
+						<div className="flex-item"><i class="fab fa-node"></i></div>
+						<div className="flex-item"><i class="fab fa-node-js"></i></div>
+						<div className="flex-item"><i class="fab fa-python"></i></div>
 					</div>
 				</Container>
 			</div>
