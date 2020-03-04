6b23a92ba2ea94e8251ffe80cc561395aac0d3fc
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-03 19:48:10 -0500

    added complementary background color

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,8 @@ import QuickInfoList from './QuickInfoList';
 
 const About = () => {
 	return (
-		<main className="about">
+		<main className="about-section">
+			<div className="bar"></div>
 			<Container fluid={true}>
 				<div className="sub-titles">
 					<h2>About</h2>
