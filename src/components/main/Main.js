721415cecf0ca7edeeb7127797385539795974e0
Kerri-Ann <kaybee1jam@yahoo.com>
2020-08-20 12:54:08 -0400

    refactor structure

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,34 +1,16 @@
 import React from 'react'
-import { Container } from 'reactstrap';
 import QuickInfoList from './QuickInfoList';
-import TechDetails from './TechDetails';
-import About from './About';
+import Details from './Details';
 import Projects from './Projects';
 import Contact from './Contact';
 
 const Main = () => {
 	return (
 		<main>
-			<section className="about-section" id="about">
-				<div className="bar"></div>
-				<Container fluid={true}>
-					<div className="sub-titles"><h2>About</h2></div>
-					<QuickInfoList />
-				</Container>
-			</section>
-
-			<section className="detailed-info">
-				<TechDetails />
-				<About />
-			</section>
-
-			<section className="projects-list" id="projects">
-				<Projects />
-			</section>
-
-			<section id="contact" className="contact-details">
-				<Contact />
-			</section>
+			<QuickInfoList />
+			<Details />
+			<Projects />
+			<Contact />
 		</main>
 	)
 }
--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -4,14 +4,14 @@ import ProjectBlock from './ProjectBlock';
 
 const Projects = () => {
 	return (
-		<>
+		<section className="projects-list" id="projects">
 			<Container>
 				<div className="sub-titles"><h2>Projects</h2></div>
 				<div className="projects-wrapper">
 					<ProjectBlock />
 				</div>
 			</Container>
-		</>
+		</section>
 	)
 }
 
