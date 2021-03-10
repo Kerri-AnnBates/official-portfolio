0d9c00739c04d21bf964879393e9455dbfa4abd4
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-03-09 21:19:47 -0500

    take down resume

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import resume from '../../assets/Resume.pdf';
 
 const Contact = () => {
 	return (
@@ -8,7 +7,6 @@ const Contact = () => {
 			<Container>
 				<div className="sub-titles"><h2>Connect With Me</h2></div>
 				<p>Feel free to reach out with any questions.</p>
-				<p>Check out my <a href={resume} target="_blank" rel="noopener noreferrer" className="resume" title="Resume">Resumé</a></p>
 				<div className="connect-links">
 					<a href="https://www.linkedin.com/in/kerri-ann-bates/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><i className="fab fa-linkedin"></i></a>
 					<a href="mailto:bates.kerriann@gmail.com" title="Email" target="_blank" rel="noopener noreferrer" ><i className="fas fa-envelope-square"></i></a>
