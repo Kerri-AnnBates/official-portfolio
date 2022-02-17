02f189c31e5a9a962c7e6cc91ffb2dccf62a7a3e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2022-02-17 10:56:04 -0500

    minor maintenance

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -9,7 +9,6 @@ const Contact = () => {
 				<p>Feel free to reach out with any questions.</p>
 				<div className="connect-links">
 					<a href="https://www.linkedin.com/in/kerri-ann-bates/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><i className="fab fa-linkedin"></i></a>
-					<a href="mailto:bates.kerriann@gmail.com" title="Email" target="_blank" rel="noopener noreferrer" ><i className="fas fa-envelope-square"></i></a>
 					<a href="https://twitter.com/KerriAnnBates7" target="_blank" rel="noopener noreferrer" title="Twitter"><i className="fab fa-twitter-square"></i></a>
 					<a href="https://github.com/Kerri-AnnBates" target="_blank" rel="noopener noreferrer" title="Github"><i className="fab fa-github-square"></i></a>
 				</div>
